5087a1543128acf90740ac25e903b81f2803593c
Miroslav Vidovic <miroslav-vidovic@hotmail.com>
2016-07-28 08:34:09 +0200

    Script creates the database

---
+++ conf/create_db.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# -------------------------------------------------------
+# Info:
+# 	Miroslav Vidovic
+# 	create_db.sh
+# 	28.07.2016.-08:17:58
+# -------------------------------------------------------
+# Description:
+#   Script creates the sqlite database for the todo app.
+# Usage:
+#   Make sure you habe sqlite3 installe and run the script
+#     bash create_db.sh
+#     ./create_db.sh
+# -------------------------------------------------------
+# Script:
+
+# TODO: Write the script to use the commands from the sql file.
+
+exit 0
