073e0ba20a4376c440b300f47943796db605ed1e
Miroslav Vidović <miroslav-vidovic@hotmail.com>
2017-03-23 15:15:15 +0100

    Includes and comments section update.

--- app/todo.sh
+++ app/todo.sh
@@ -6,20 +6,18 @@
 #   file:      todo.sh
 #   created:   26.07.2016.-15:32:34
 #   revision:  22.03.2016.
-#   version:   1.1
+#   version:   2.0
 # -----------------------------------------------------------------------------
 # Requirements:
 #   sqlite3, zenity
 # Description:
-#   Task application (todo app) made with sqlite
+#   Task application (todo app) made with sqlite, bash and zenity.
 # Usage:
 #   todo.sh
-#
 # -----------------------------------------------------------------------------
 # Script:
 
 # Includes
-source ./functions/colors.sh
 source ./functions/sqlite-queries.sh
 source ./functions/task.sh
 source ./functions/zenity-guis.sh
@@ -28,73 +26,4 @@ source ./functions/helpers.sh
 # Path to database
 database=../database/todo.sqlite
 
-# Show help for the user
-help(){
-  cat<< heredoc
-  Todo app - task manager
-
-  Usage : $(basename "$0") [options]
-
-  Options:
-    -a         Display all active tasks
-    -c id      Mark a task with selected "id" as completed
-    -d id      Delete a task with "id"
-    -f         Display all finished tasks
-    -g id      Send a task with "id" to Google calendar - requires gcalcli
-    -n         Insert a new task via a Zenity form
-    -h         Display this help message  
-    -t id      Display details for task with "id"
-heredoc
-}
-
-main(){
-  while getopts 'ac:d:fg:hnt:' flag; do
-    case "${flag}" in
-      a)
-        # Select all active tasks
-        select_all_active
-        ;;
-      c)
-        # Mark a task as completed
-        id=${OPTARG}
-        complete_task "$id"
-        ;;
-      d)
-        # Delete a task from the database
-        id=${OPTARG}
-        delete_task "$id"
-        ;;
-      f)
-        # Show all completed(finished) tasks
-        select_all_completed
-        ;;
-      g)
-      # Send a task to google calendar
-        id=${OPTARG}
-        select_one_task "$id"
-        google_calendar
-        ;;
-      n)
-        # Add a new task form a zenity form
-        # Get the data from the form
-        data=$(insert_form)
-        # Save the task
-        insert_task $data
-        ;;
-      h)
-        help
-        ;;
-      t)
-        # One task detailed view
-        id=${OPTARG}
-        select_one_task "$id"
-        ;;
-      *) error "Unexpected option ${flag}" ;;
-    esac
-  done
-}
-
-check_for_empty_input "$@"
-main "$@"
-
-exit 0
+main_menu
