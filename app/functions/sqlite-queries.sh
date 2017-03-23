f66df5db919e6c5f5fd7bdf86991a98f762fafbf
Miroslav Vidović <miroslav-vidovic@hotmail.com>
2017-03-23 13:53:44 +0100

    Global array comment section

--- app/functions/sqlite-queries.sh
+++ app/functions/sqlite-queries.sh
@@ -1,48 +1,54 @@
 #!/bin/sh
 
-database=../database/todo.sqlite
-
 # Insert function
-# @param $1 - description
-# @param $2 - created_date
-# @param $3 - due_date
-# @param $4 - completed_date
-# @param $5 - project
+# $1 - description
+# $2 - created_date
+# $3 - due_date
+# $4 - completed_date
+# $5 - project
 database_insert(){
-  # Save the data to the database
   sqlite3 $database "insert into tasks (description, created_date, due_date, completed, project)
                        values ($1, $2, $3, $4, $5)"
 }
 
 # Update function
+# $1 - id
+# $2 - description
+# $3 - created_date
+# $4 - due_date
+# $5 - completed_date
+# $6 - project
 database_update(){
-  sqlite3 $database "update tasks set description=$description, created_date=$created_date,
-                     due_date=$due_date, completed=$completed, project=$project where id=$id"
+  sqlite3 $database "update tasks set description=$2, created_date=$3,
+                     due_date=$4, completed=$5, project=$6 where id=$1"
 }
 
 # Update completed field to 1
-# @param $1 - id
+# $1 - id
 database_task_completed(){
   sqlite3 $database "update tasks set completed=1 where id=$1"
 }
 
 # Delete a task
-# @param $1 - id
-database_task_delete(){
+# $1 - id
+database_delete(){
   sqlite3 $database "DELETE FROM tasks WHERE id=$1"
 }
 
-database_task_select_one() {
-  data=$(sqlite3 $database "select * from tasks where id=$id")
-  echo $data
+# Select one task
+# $1 - id
+# creates a global array - one_task_array
+database_select_one() {
+  declare -ga one_task_array
+  one_task_array=($(sqlite3 $database "select * from tasks where id=$1"))
 }
 
+# Select all tasks
+# creates a global array - all_tasks_array
 database_select_all() {
-  declare -ga data_array
-  if [[ $1 == 1 ]]; then
-    data_array=($(sqlite3 $database "select id, description, created_date, due_date, project from tasks where completed = 1"))
-  else
-    data_array=($(sqlite3 $database "select id, description, created_date, due_date, project from tasks where completed = 0"))
-  fi
+  # array separator
+  IFS=$'\n'
+  declare -ga all_tasks_array
+  all_tasks_array=($(sqlite3 $database "select id, description, created_date, due_date, project, completed from tasks"))
 }
 
